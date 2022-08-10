104263417f34f97bc70b735d30c845044605a84f
TojeMneCiceron <skorobogatova.mm5910@gmail.com>
2022-08-10 23:01:30 +0500

    final

---
+++ mpi_lab1/mpi_lab1.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include "mpi.h"
+using namespace std;
+
+const double A = 0, B = sqrt(3), E = 1;
+
+double F(double s)
+{
+    return s / sqrt(4 - s * s);
+}
+
+double Calc(int bg, int ed, double h)
+{
+    double sum = 0;
+
+    for (int i = bg; i < ed; i++)
+    {
+        double s = A + h * i;
+        sum += F(s);
+    }
+
+    return h * sum;
+}
+
+void GetIndices(int rank, int np, int &bg, int &ed)
+{   
+    bg = np * rank;
+    ed = np + bg;
+}
+
+int main(int argc, char** argv)
+{
+    MPI_Status status;
+	MPI_Init(&argc, &argv);
+    double time = MPI_Wtime();
+
+    int rank, size;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int n = 1000000;
+    int bg, ed;
+    GetIndices(rank, n / size, bg, ed);
+
+    double res = Calc(bg, ed, (B - A) / n);
+
+    double sum;    
+    MPI_Reduce(&res, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    time = MPI_Wtime() - time;
+
+    double max_time;
+    MPI_Reduce(&time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    
+    if (rank == 0)
+        cout << "time = " << max_time << "\nsum = " << sum << "\ne = " << abs(sum - E) / E;
+    
+	MPI_Finalize();
+	return 0;
+}
---
+++ nompi/nompi.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+const double A = 0, B = sqrt(3), E = 1;
+
+double F(double s)
+{
+    return s / sqrt(4 - s * s);
+}
+
+double Calc(int bg, int ed, double h)
+{
+    double sum = 0;
+
+    for (int i = bg; i < ed; i++)
+    {
+        double s = A + h * i;
+        sum += F(s);
+    }
+
+    return h * sum;
+}
+
+int main()
+{
+    double time = clock();
+    int n = 1000000;
+
+    double res = Calc(0, n, (B - A) / n);
+
+    //double error = abs(res - E) / E;
+
+    time = clock() - time;
+    cout << "time = " << time / CLOCKS_PER_SEC << "\nsum = " << res << "\ne = " << abs(res - E) / E;
+
+}
